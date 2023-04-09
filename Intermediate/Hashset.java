c0a8b35450ea427d24c7493d1dbacdf47afcc2cd
MochaDev <michaelmcgarvey03@gmail.com>
2023-04-09 16:56:35 -0400

    Intermediate section

---
+++ Intermediate/Hashset.java
@@ -0,0 +1,72 @@
+package Intermediate;
+import java.util.HashSet; // We have to import this to use a "HashSet"
+
+// What you'll learn:
+// How to create and work with a HashSet inside Java.
+
+// Simplified definition:
+// A hashset holds a set of items where each item is unique,
+// meaning two of the same item can't be inside a hashet.
+
+public class Hashset
+{
+    // To start off with a HashSet, it needs to be created first
+
+    // For example:
+    // HashSet<String> hashy = new HashSet<>();
+    // This will create a HashSet with the name "hashy" which will take in Strings
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // To add anything inside of a HashSet, you have to use the "add" keyword
+
+    // For example:
+    // hashy.add("Food");
+    // "hashy" will now contain the word "Food"
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // To check if a word is contained inside of a HashSet, you use the "contains" keyword
+
+    // For example:
+    // hashy.contains("Food");
+    // This would return TRUE since "Food" is contained inside the HashSet
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // To remove anything from a HashSet, you have to use the "remove" keyword
+
+    // For example:
+    // hashy.remove("Food");
+    // This would remove the word "Food" from the HashSet
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // Lastly, if you try to add in a word that the HashSet already contains,
+    // it won't make a new word, but replace the old word with the new word
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    // We will now print out a HashSet after using all of the functions above
+    public static void main(String[] args)
+    {
+        HashSet<String> hashy = new HashSet<>();
+
+        hashy.add("Food");
+
+        System.out.println("Contains the word Food?: " + hashy.contains("Food"));
+
+        hashy.add("Chicken");
+        hashy.add("Steak");
+        hashy.add("Food");
+
+        hashy.remove("Steak");
+
+        System.out.println(hashy);
+    }    
+}
---
+++ Intermediate/TwoDimensionalArray.java
@@ -0,0 +1,84 @@
+package Intermediate;
+
+// What you'll learn:
+// How to create and use two-dimensional array inside Java.
+
+public class TwoDimensionalArray 
+{
+    // A two-dimensional array (2d-array) does the same as an array but in two-dimensions
+    
+    // For example (Array):
+    // int[] arr = new int[5];
+    // This creates an array of 5 int's
+
+    // This is how it looks
+    // [0, 0, 0, 0, 0] <-- Int array's are initialized with 0's if not told otherwise
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // For example (Two-dimensonal array)
+    // int[][] arr = new int[5][5];
+    // This creates a 2d array of 25 int's
+
+    // This is how it looks
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0] <-- Int array's are initialized with 0's if not told otherwise
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // You can access index inside 2d array's the same as 1d array's but having to use two index values
+    
+    // In this example, that would look like
+    // arr[0][0] = 1;
+    // (The first index is always going to the index of the ROW, and the second is the COLUMN)
+    
+    // Now this is the new array
+    // [1, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+
+    // Let's try another one
+    // arr[3][3] = 2;
+
+    // Now this is the new array
+    // [1, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 0, 0]
+    // [0, 0, 0, 2, 0]
+    // [0, 0, 0, 0, 0]
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // To get the length of this 2d array in code, you use [ARRAY_NAME].length
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    // This function will print out the values of the 2d array we went over previously
+    
+    public static void main(String[] args)
+    {
+        int[][] arr = new int[5][5];
+    
+        arr[0][0] = 1;
+        arr[3][3] = 2;
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = 0; j < arr.length; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
